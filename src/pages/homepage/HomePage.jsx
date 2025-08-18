199f186717f2013b22e7d061f89754adc9d06890
math <mathieurais@gmail.com>
2025-08-18 14:32:15 +0200

    Page de création de quiz + traductions. Pas de sauvegardes pour le moment

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { FlaskConical, Plus } from "lucide-react";
 import styled from "styled-components";
