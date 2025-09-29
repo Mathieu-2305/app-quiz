d7edea278a882ca540f357ac65a9f5a75d35e080
a.berner <aberner1802@gmail.com>
2025-09-29 07:23:39 +0200

    Redone API Laravel

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,15 +14,17 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', '*')),
 
-    'allowed_origins' => ['http://localhost:5174'], // Your React dev server
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
 
-    'allowed_headers' => ['*'],
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', '*')),
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', false),
 ];
